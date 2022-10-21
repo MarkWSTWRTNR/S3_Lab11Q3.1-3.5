7ef18b4bcfac72d59726f3604374c52f5ca65e0d
markwst <Wongsathorn_W@gmail.com>
2022-10-21 08:36:55 +0700

    3.5

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <label v-if="label">{{ label }}</label>
+  <input
+    :value="modelValue"
+    :placeholder="label"
+    @input="inputHandler($event)"
+    v-bind="$attrs"
+    class="field"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    }
+  },
+  methods: {
+    inputHandler(event) {
+      this.$emit('update:modelValue', event.target.value)
+    }
+  }
+}
+</script>
+<style scoped>
+label {
+  color: rgb(0, 0, 0);
+}
+input {
+  border-radius: 50px;
+  border: 1px solid #000000;
+}
+</style>
---
+++ src/components/EventCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="event-card">
+    <span>Description : {{ auction.description }} </span>
+    <h5>Type: {{ auction.type }}</h5>
+    <h5>
+      Successful Bid : {{ auction.successfulBid.id }} Amount :{{
+        auction.successfulBid.amount
+      }}
+    </h5>
+  </div>
+</template>
+<script>
+export default {
+  name: 'EventCard',
+  props: {
+    auction: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  background: white;
+  border: 1px solid #000000;
+  margin-bottom: 18px;
+  transition: 0.25s;
+  border-radius: 20px;
+}
+
+.event-card:hover {
+  background: transparent;
+  color: rgb(20, 41, 0);
+  transform: scale(1.01);
+  border-radius: 20px;
+  box-shadow: 0 3px 12px 0 rgba(10, 10, 10, 0.753);
+}
+
+.event-link {
+  color: #2c3e50;
+  text-decoration: none;
+}
+</style>
